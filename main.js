85501fefac0a4e16479faa1028fc51e144309263
arshsiddiqui <arshsiddiqui@Vt.edu>
2021-11-12 12:10:36 -0500

    Messed around with JS

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("How about this crazy weather we're having today?")
+function formdata(){
+	var streetAddress = document.getElementById("address").value();
+	var country = document.getElementById("country").value();
+	findSound();
+}
+
+function findSound(){
+	var audio = new Audio('/sounds/CSLounge.M4A');
+	audio.play();
+}
