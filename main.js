2a855d1bc7ca38c7d827e6f22903141d026a038b
arshsiddiqui <arshsiddiqui@gmail.com>
2022-06-03 10:57:29 -0400

    Development of linear and fraction-of search

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 function formdata(){
 	console.log("Ici au moins");
 	let town = document.getElementById("town").value;
-	console.log(town);
 	//var country = document.getElementById("country").value();
 	findSound(town);
 }
 
 function findSound(town){
 	console.log("To play audio");
-	var audio = new Audio('sounds/' + town + '.m4a');//CSLounge
-	audio.play();
+	const audioOptions = ["CSLounge"];
+	const options = [];
+	if (audioOptions.includes(town)) {
+		console.log("There carte blanche");
+		var audio = new Audio('sounds/' + town + '.m4a');//CSLounge
+		audio.play();
+	} else {
+		for (let i = 0; i < audioOptions.length; i++) {
+			if (audioOptions[i].includes(town)) {
+				options.push(audioOptions[i]);
+			}
+		}
+		console.log("OPTIONS: " + options[0] + ".m4a");
+		var audio = new Audio('sounds/' + options[0] + '.m4a');//CSLounge
+		audio.play();
+	}
 	console.log("Playing audio");
 }
